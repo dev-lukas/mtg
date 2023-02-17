b3329cfea3897f8622871a3ae3bccc09e98c8852
dev-lukas <l.b.roth@t-online.de>
2023-02-17 17:24:22 +0100

    Added favicon

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="de">
 <head>
     <title>MTG Companion</title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
